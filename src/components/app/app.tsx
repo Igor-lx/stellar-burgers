4095d7549046b196fe9e5c92cfb3ff0d4e77c23e
Igor-lx <lorax.k@yandex.ru>
2025-04-08 13:53:43 +0200

    bug fixes + slight  routing changes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { AppHeader } from '../app-header';
 import { IngredientDetails } from '../ingredient-details';
 import { Modal } from '../modal';
@@ -17,111 +17,117 @@ import {
   NotFound404
 } from '@pages';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
+const App = () => {
+  const location = useLocation();
+  const background = location.state?.background;
 
-    <Routes>
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+
       {/* общие роуты */}
-      <Route path='/' element={<ConstructorPage />} />
-      <Route path='/feed' element={<Feed />} />
+      <Routes location={background || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
 
-      {/* роуты модалок */}
-      <Route path='/feed/:number' element={<OrderInfo />} />
-      <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        {/* роуты модалок */}
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
 
-      {/* защищенные роуты */}
-      <Route
-        path='/login'
-        element={
-          <ProtectedRoute>
-            <Login />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/register'
-        element={
-          <ProtectedRoute>
-            <Register />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/forgot-password'
-        element={
-          <ProtectedRoute>
-            <ForgotPassword />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/reset-password'
-        element={
-          <ProtectedRoute>
-            <ResetPassword />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/profile'
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/profile/orders'
-        element={
-          <ProtectedRoute>
-            <ProfileOrders />
-          </ProtectedRoute>
-        }
-      />
-
-      {/* защищенные роуты модалок */}
-      <Route
-        path='/profile/orders/:number'
-        element={
-          <ProtectedRoute>
-            <Modal title='' onClose={() => {}}>
-              <OrderInfo />
-            </Modal>
-          </ProtectedRoute>
-        }
-      />
-
-      {/* Модалки в зависимости от бэкграунда */}
-      <Routes>
+        {/* защищенные роуты */}
         <Route
-          path='/feed/:number'
+          path='/login'
           element={
-            <Modal title='' onClose={() => {}}>
-              <OrderInfo />
-            </Modal>
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
           }
         />
         <Route
-          path='/ingredients/:id'
+          path='/register'
           element={
-            <Modal title='' onClose={() => {}}>
-              <IngredientDetails />
-            </Modal>
+            <ProtectedRoute>
+              <Register />
+            </ProtectedRoute>
           }
         />
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRoute>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <ProtectedRoute>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRoute>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* защищенные роуты модалок */}
         <Route
           path='/profile/orders/:number'
           element={
-            <Modal title='' onClose={() => {}}>
+            <ProtectedRoute>
               <OrderInfo />
-            </Modal>
+            </ProtectedRoute>
           }
         />
+
+        <Route path='*' element={<NotFound404 />} />
       </Routes>
-      <Route path='*' element={<NotFound404 />} />
-    </Routes>
-  </div>
-);
+
+      {/* Модалки в зависимости от бэкграунда */}
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='' onClose={() => {}}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
-    publicPath: '/св'
+    publicPath: '/'
   },
   devServer: {
     static: path.join(__dirname, './dist'),
