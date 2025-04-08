b2ca9a00237720e9029fd3ae57b797218c466bd2
Igor-lx <lorax.k@yandex.ru>
2025-04-08 20:19:36 +0200

    slices v4

---
+++ src/store/slices/orderSlice.ts
@@ -0,0 +1,81 @@
+import {
+  orderBurgerApi,
+  getOrderByNumberApi,
+  TNewOrderResponse,
+  TOrderResponse
+} from '@api';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { clearConstructor } from './burgerConstructorSlice';
+import { TOrder } from '@utils-types';
+
+export const fetchOrder = createAsyncThunk(
+  'order/fetch',
+  async (ids: string[], { dispatch }) => {
+    dispatch(clearConstructor());
+    return await orderBurgerApi(ids);
+  }
+);
+
+export const fetchOrderByNum = createAsyncThunk(
+  'order/fetchByNum',
+  async (orderId: number) => await getOrderByNumberApi(orderId)
+);
+
+type OrderState = {
+  fetchStatus: 'idle' | 'loading' | 'succeeded' | 'failed';
+  order: TOrder | null;
+};
+
+const initialState: OrderState = {
+  fetchStatus: 'idle',
+  order: null
+};
+
+const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {
+    clearOrder: (state) => {
+      state.order = null;
+      state.fetchStatus = 'idle';
+    }
+  },
+  selectors: {
+    selectOrder: (state: OrderState) => state.order,
+    selectOrderStatus: (state: OrderState) => state.fetchStatus,
+    selectOrderLoading: (state: OrderState) => state.fetchStatus === 'loading'
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchOrder.pending, (state) => {
+        state.fetchStatus = 'loading';
+      })
+      .addCase(fetchOrder.rejected, (state) => {
+        state.fetchStatus = 'failed';
+      })
+      .addCase(
+        fetchOrder.fulfilled,
+        (state, action: PayloadAction<TNewOrderResponse>) => {
+          state.fetchStatus = 'succeeded';
+          state.order = action.payload.order;
+        }
+      )
+      .addCase(fetchOrderByNum.pending, (state) => {
+        state.fetchStatus = 'loading';
+      })
+      .addCase(fetchOrderByNum.rejected, (state) => {
+        state.fetchStatus = 'failed';
+      })
+      .addCase(
+        fetchOrderByNum.fulfilled,
+        (state, action: PayloadAction<TOrderResponse>) => {
+          state.fetchStatus = 'succeeded';
+          state.order = action.payload.orders[0];
+        }
+      );
+  }
+});
+
+export const { clearOrder } = orderSlice.actions;
+export const { selectOrder, selectOrderStatus, selectOrderLoading } =
+  orderSlice.selectors;
--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,27 +1,111 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {
+  getUserApi,
+  TRegisterData,
+  registerUserApi,
+  TLoginData,
+  TUserResponse,
+  loginUserApi,
+  updateUserApi,
+  logoutApi
+} from '@api';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TUser } from '@utils-types';
 
-interface UserState {
-  ifUserAuthenticated: boolean;
-  userInfo: TUser | null;
-}
-
-const initialState: UserState = {
-  ifUserAuthenticated: false,
-  userInfo: null
+type UserState = {
+  ready: boolean;
+  loading: boolean;
+  userData: TUser | null;
 };
 
-const userSlice = createSlice({
+const initialState: UserState = {
+  ready: false,
+  loading: false,
+  userData: null
+};
+
+export const fetchUser = createAsyncThunk(
+  'user/fetchUser',
+  async () => await getUserApi()
+);
+
+export const signUp = createAsyncThunk(
+  'user/signUp',
+  async (form: TRegisterData) => await registerUserApi(form)
+);
+
+export const signIn = createAsyncThunk(
+  'user/signIn',
+  async (form: TLoginData) => await loginUserApi(form)
+);
+
+export const editUser = createAsyncThunk(
+  'user/editUser',
+  async (form: TRegisterData) => await updateUserApi(form)
+);
+
+export const signOut = createAsyncThunk(
+  'user/signOut',
+  async () => await logoutApi()
+);
+
+export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
   selectors: {
-    selectIfUserAuthenticated: (state: UserState) => state.ifUserAuthenticated,
-    selectUserInfo: (state: UserState) => state.userInfo
+    selectUserData: (s: UserState) => s.userData,
+    selectReady: (s: UserState) => s.ready,
+    selectUserName: (s: UserState) => s.userData?.name || ''
+  },
+  extraReducers: (builder) => {
+    const onPending = (state: UserState) => {
+      state.loading = true;
+      state.ready = false;
+    };
+
+    const onRejected = (state: UserState) => {
+      state.loading = false;
+      state.ready = true;
+    };
+
+    const onSuccess = (
+      state: UserState,
+      action: PayloadAction<TUserResponse>
+    ) => {
+      state.loading = false;
+      state.ready = true;
+      state.userData = action.payload.user;
+    };
+
+    const onSignOutSuccess = (state: UserState) => {
+      state.userData = null;
+      state.loading = false;
+      state.ready = true;
+    };
+
+    builder
+      .addCase(signUp.pending, onPending)
+      .addCase(signUp.rejected, onRejected)
+      .addCase(signUp.fulfilled, onSuccess)
+
+      .addCase(signIn.pending, onPending)
+      .addCase(signIn.rejected, onRejected)
+      .addCase(signIn.fulfilled, onSuccess)
+
+      .addCase(fetchUser.pending, onPending)
+      .addCase(fetchUser.rejected, onRejected)
+      .addCase(fetchUser.fulfilled, onSuccess)
+
+      .addCase(editUser.pending, onPending)
+      .addCase(editUser.rejected, onRejected)
+      .addCase(editUser.fulfilled, onSuccess)
+
+      .addCase(signOut.pending, onPending)
+      .addCase(signOut.rejected, onRejected)
+      .addCase(signOut.fulfilled, onSignOutSuccess);
   }
 });
 
-export const { selectIfUserAuthenticated, selectUserInfo } =
+export const { selectUserData, selectReady, selectUserName } =
   userSlice.selectors;
-
 export default userSlice.reducer;
