86c1e11ba0ea4179ff23b0ed894d0949342ff710
Igor-lx <lorax.k@yandex.ru>
2025-04-12 10:35:22 +0200

    pre-loader fix

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
 import { useAppSelector } from '../../store/hooks';
+import { Preloader } from '../ui';
 
 export type ProtectedRouteProps = {
   publicRoute?: boolean;
@@ -12,8 +13,13 @@ export const ProtectedRoute: FC<ProtectedRouteProps> = ({
   publicRoute: anonymous = false
 }) => {
   const isLoggedIn = useAppSelector((store) => store.user.ifAuth);
+  const isLoading = useAppSelector((store) => store.user.loading);
   const location = useLocation();
   const from = location.state?.from || '/';
+
+  if (isLoading) {
+    return <Preloader />;
+  }
 
   if (anonymous && isLoggedIn) {
     return <Navigate to={from} />;
