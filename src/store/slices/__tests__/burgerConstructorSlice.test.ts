35f099f9418bbf9f70598dadb78ab45e155746db
Igor-lx <lorax.k@yandex.ru>
2025-04-16 11:04:42 +0200

    constructor test slight fixes

--- src/store/slices/__tests__/burgerConstructorSlice.test.ts
+++ src/store/slices/__tests__/burgerConstructorSlice.test.ts
@@ -1,11 +1,12 @@
 import { generateIngredient } from '../../../utils/test-utils';
-import { TConstructorIngredient, TIngredient } from '../../../utils/types';
+import { TIngredient, TConstructorIngredient } from '../../../utils/types';
 import {
   burgerConstructorSlice,
   addIngredient,
   removeIngredient,
   moveIngredient,
-  clearConstructor
+  clearConstructor,
+  BurgerConstructorState
 } from '../burgerConstructorSlice';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -18,9 +19,18 @@ describe('burgerConstructorSlice', () => {
   const ingredient1: TIngredient = generateIngredient('Test Sauce', 'sauce');
   const ingredient2: TIngredient = generateIngredient('Test Main', 'main');
 
+  let initialStateSample: BurgerConstructorState;
+
+  beforeEach(() => {
+    initialStateSample = {
+      bun: null,
+      ingredients: []
+    };
+  });
+
   it('добавление булки', () => {
     const state = burgerConstructorSlice.reducer(
-      { bun: null, ingredients: [] },
+      initialStateSample,
       addIngredient({ ...bunSample, id: uuidv4() } as TConstructorIngredient)
     );
     expect(state.bun).toEqual({ ...bunSample, id: uuidv4() });
@@ -29,7 +39,7 @@ describe('burgerConstructorSlice', () => {
 
   it('добавление ингредиента', () => {
     const state = burgerConstructorSlice.reducer(
-      { bun: null, ingredients: [] },
+      initialStateSample,
       addIngredient({
         ...ingredient1,
         id: uuidv4()
@@ -43,30 +53,28 @@ describe('burgerConstructorSlice', () => {
   });
 
   it('удаление ингредиента', () => {
-    const initialState = {
-      bun: null,
-      ingredients: [
-        { ...ingredient1, id: uuidv4() },
-        { ...ingredient2, id: uuidv4() }
-      ]
-    };
     const state = burgerConstructorSlice.reducer(
-      initialState,
+      {
+        bun: null,
+        ingredients: [
+          { ...ingredient1, id: uuidv4() },
+          { ...ingredient2, id: uuidv4() }
+        ]
+      },
       removeIngredient(0)
     );
     expect(state.ingredients).toEqual([{ ...ingredient2, id: uuidv4() }]);
   });
 
   it('перемещение ингредиента', () => {
-    const initialState = {
-      bun: null,
-      ingredients: [
-        { ...ingredient1, id: uuidv4() },
-        { ...ingredient2, id: uuidv4() }
-      ]
-    };
     const state = burgerConstructorSlice.reducer(
-      initialState,
+      {
+        bun: null,
+        ingredients: [
+          { ...ingredient1, id: uuidv4() },
+          { ...ingredient2, id: uuidv4() }
+        ]
+      },
       moveIngredient({ fromIndex: 0, toIndex: 1 })
     );
     expect(state.ingredients).toEqual([
@@ -76,17 +84,16 @@ describe('burgerConstructorSlice', () => {
   });
 
   it('очистка конструктора', () => {
-    const initialState = {
-      bun: { ...bunSample, id: uuidv4() },
-      ingredients: [
-        { ...ingredient1, id: uuidv4() },
-        { ...ingredient2, id: uuidv4() }
-      ]
-    };
     const state = burgerConstructorSlice.reducer(
-      initialState,
+      {
+        bun: { ...bunSample, id: uuidv4() },
+        ingredients: [
+          { ...ingredient1, id: uuidv4() },
+          { ...ingredient2, id: uuidv4() }
+        ]
+      },
       clearConstructor()
     );
-    expect(state).toEqual({ bun: null, ingredients: [] });
+    expect(state).toEqual(initialStateSample);
   });
 });
